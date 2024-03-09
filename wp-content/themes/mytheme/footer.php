39691d199fdb6f7ef00fdb605dd0b78f45c93c50
SofNyb <sofi13a5@edu.zealand.dk>
2024-03-09 09:35:59 +0100

    Ændret footer+header-farve, tilføjet link til mail + mere

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,5 +1,8 @@
     <footer>    
-        <p>This is my footer</p>
+        <p>
+            <a class="footer-item" href="mailto:sofi13a5@edu.zealand.dk">Send mig en mail</a>
+        </p>
+        <p class="footer-item">Copyright: Sofie Windeløv</p>
     </footer>
 </body>
 </html>
