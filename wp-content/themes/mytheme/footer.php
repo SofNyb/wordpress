62410951467555288d75b00d88859ce386861df2
SofNyb <sofi13a5@edu.zealand.dk>
2024-03-09 12:45:57 +0100

    Tilføjet billeder og ændret i styling på footer

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,8 +1,7 @@
-    <footer>    
-        <p>
-            <a class="footer-item" href="mailto:sofi13a5@edu.zealand.dk">Send mig en mail</a>
+    <footer>
+        <p class="footer-item">Copyright: Sofie Windeløv
+            <a href="http://localhost:8000/om-mig/">Send mig en mail</a>
         </p>
-        <p class="footer-item">Copyright: Sofie Windeløv</p>
     </footer>
 </body>
 </html>
